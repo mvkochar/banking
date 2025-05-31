1823a8fb8fde53ad7744be793200391390864551
sgkochar <sgkochar@gmail.com>
2025-05-31 12:53:11 +0300

    add Faq, Testimonials and CTA blocks to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -263,17 +263,88 @@ const Home = () => {
           <div className="home-faq-item">
             <h5 className="faq-item-question">Are my transactions and personal information secure?</h5>
             <p className="faq-item-answer">
-              At YourBank, we prioritize the security of your transactions and personal information. 
-              We employ industry-leading encryption and multi-factor authentication to ensure that 
-              your data is protected. Additionally, we regularly update our security measures to stay 
-              ahead of emerging threats. You can bank with confidence knowing that we have robust 
+              At YourBank, we prioritize the security of your transactions and personal information.
+              We employ industry-leading encryption and multi-factor authentication to ensure that
+              your data is protected. Additionally, we regularly update our security measures to stay
+              ahead of emerging threats. You can bank with confidence knowing that we have robust
               security systems in place.
             </p>
           </div>
         </div>
         <button className='home-faq-load'>Load All FAQ's</button>
       </section>
-  
+      <section className='home-testimonials'>
+        <div className="d-f jc-sb">
+          <div>
+            <h2 className="page-bl-title"><span>Our</span> Testimonials</h2>
+            <p className="page-bl-desc">
+              Discover how YourBank has transformed lives with innovative digital solutions
+              and personalized customer service. See why our clients trust us for a secure
+              and prosperous financial journey
+            </p>
+          </div>
+          <div className="home-testimonials-tabs d-f">
+            <button className='tab-btn tab-btn__active'>For Individuals</button>
+            <button className='tab-btn'>For Businesses</button>
+          </div>
+        </div>
+        <div className="home-testimonials-content d-f align-center">
+          <button className='btn-clear d-b'>
+            <svg width="62" height="62" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect x="0.5" y="0.5" width="61" height="61" rx="30.5" fill="#1C1C1C" />
+              <rect x="0.5" y="0.5" width="61" height="61" rx="30.5" stroke="#262626" />
+              <path d="M40.2083 31H22.5M22.5 31L31 22.5M22.5 31L31 39.5" stroke="#CAFF33" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+          </button>
+          <div className="home-testimonials-box d-f">
+            <div className="home-testimonials-item">
+              <div><img src="/images/quote.png" alt="quote" /></div>
+              <p className="testimonials-item-desc">
+                YourBank has been my trusted financial partner for years.
+                Their personalized service and innovative digital banking
+                solutions have made managing my finances a breeze.
+              </p>
+              <h5 className="testimonials-item-name">Sara T</h5>
+            </div>
+            <div className="home-testimonials-item">
+              <div><img src="/images/quote.png" alt="quote" /></div>
+              <p className="testimonials-item-desc">
+                I recently started my own business, and YourBank has been instrumental
+                in helping me set up my business accounts and secure the financing I needed.
+                Their expert guidance and tailored solutions have been invaluable.
+              </p>
+              <h5 className="testimonials-item-name">John D</h5>
+            </div>
+            <div className="home-testimonials-item">
+              <div><img src="/images/quote.png" alt="quote" /></div>
+              <p className="testimonials-item-desc">
+                I love the convenience of YourBank's mobile banking app.
+                It allows me to stay on top of my finances and make transactions on the go.
+                The app is user-friendly and secure, giving me peace of mind.
+              </p>
+              <h5 className="testimonials-item-name">Emily G</h5>
+            </div>
+          </div>
+          <button className='btn-clear d-b'>
+            <svg width="62" height="62" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect x="0.5" y="0.5" width="61" height="61" rx="30.5" fill="#1C1C1C" />
+              <rect x="0.5" y="0.5" width="61" height="61" rx="30.5" stroke="#262626" />
+              <path d="M22.5002 31H40.2085M40.2085 31L31.7085 22.5M40.2085 31L31.7085 39.5" stroke="#CAFF33" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+          </button>
+        </div>
+      </section>
+      <section className='home-cta d-f jc-sb align-center'>
+        <div>
+          <h2 className="page-bl-title"><span>Start your financial journey with</span> YourBank today!</h2>
+          <p className="page-bl-desc">
+            Ready to take control of your finances? Join YourBank now, 
+            and let us help you achieve your financial goals with our 
+            tailored solutions and exceptional customer service
+          </p>
+        </div>
+        <a href="" className='home-cta-open'>Open Account</a>
+      </section>
     </>
   )
 }
